cc189316f454ba389a3bc18c04af5830bd755688
Xlrcrash1 <60253517+Xlrcrash1@users.noreply.github.com>
2020-10-20 00:58:25 -0700

    Added Car details to the User Registration Form

--- register.php
+++ register.php
@@ -20,6 +20,12 @@ echo "              <h5>UserName: <input type = 'text' placeholder = 'XLR8' name
 echo "              <h5>Email: <input type = 'email' placeholder = 'email@email.com' name = 'email'></h5>\n";
 echo "              <h5>Password: <input type = 'password' placeholder = '*****' name = 'password'></h5>\n";
 echo "              <h5>Confirm Password: <input type = 'password' placeholder = '*****' name = 'confirm_password'></h5>\n";
+
+echo "              <h5>Make: <input type = 'text' placeholder = 'Toyota' name = 'make'></h5>\n";
+echo "              <h5>Model: <input type = 'text' placeholder = 'GT86' name = 'model'></h5>\n";
+echo "              <h5>2017: <input type = 'number' placeholder = '2017' name = 'year'></h5>\n";
+echo "              <h5>Last 4 of LicensePlate: <input type = 'number' placeholder = 'C777' name = 'licensePlate'></h5>\n";
+echo "              <h5>Color: <input type = 'text' placeholder = 'Grey' name = 'color'></h5>\n";
 echo "              <button class = 'nav_btn' type = 'submit'>Create</button>\n";
 echo "          </form>\n";
 echo "      </div>\n";
@@ -30,12 +36,19 @@ echo "</html>\n";
 
 if (!empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])){
 
+    //this converts the post parameters from HTML into PHP parameters as well as we sanatize it to avoid sql queries being interjected
     $name = htmlspecialchars(trim($_POST['name']));/////////////////ADD SANATIZATION
     $lname = htmlspecialchars(trim($_POST['lname']));
     $username = htmlspecialchars(trim($_POST['username']));/////////////////ADD SANATIZATION
     $email = htmlspecialchars(trim($_POST['email']));
     $password = htmlspecialchars(trim($_POST['password']));/////////////////ADD SANATIZATION
     $confirm_password = htmlspecialchars(trim($_POST['confirm_password']));
+
+    $make = htmlspecialchars(trim($_POST['make']));/////////////////ADD SANATIZATION
+    $model = htmlspecialchars(trim($_POST['model']));
+    $year = htmlspecialchars(trim($_POST['year']));/////////////////ADD SANATIZATION
+    $licensePlate = htmlspecialchars(trim($_POST['licensePlate']));
+    $color = htmlspecialchars(trim($_POST['color']));
 
     //$name = $_POST['name'];
     //$lname = $_POST['lname'];
@@ -44,33 +57,37 @@ if (!empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['username
     //$password = $_POST['password'];
     //$confirm_password = $_POST['confirm_password'];
 
-    //echo "name: $name , lastname: $lname, username: $username , email: $email , password: $password , confirm: $confirm_password";
+    //echo "name: $name , lastname: $lname, username: $username , email: $email , password: $password , confirm: $confirm_password, Make: $make, Model = $model, Year: $year, licenseplate: $licensePlate";
 
     if ($password == $confirm_password){
 
-        $sql = "select * from ParkInLot_Users where username = '$username' or email = '$email'";
+        $sql = "select * from ParkInLot_Users where username = '$username' or email = '$email' or licensePlate = '$licensePlate'";
         if ($res = $db->query($sql)){
 
             $row = $res->FETCH_ASSOC();
             
             $exists = '';
             
-            if ($row['user_name'] == $username){
+            if ($row['username'] == $username){
 
                 $exists .= 'UserName taken<br>';
             }
-            if ($row['user_email'] == $email){
+            if ($row['email'] == $email){
 
                 $exists .= 'Email already exists<br>';
             }
-            if ($row['user_name'] == $username or $row['user_email'] == $email){
+            if ($row['licensePlate'] == $licensePlate){
+
+                $exists .= 'License Plate already exists<br>';
+            }
+            if ($row['user_name'] == $username or $row['user_email'] == $email or $row['licensePlate'] == $licensePlate){
 
                 echo $exists;
             }
             else{
 
                 //echo "email or username is not taken<br>\n";
-                $sql = "insert into ParkInLot_Users (firstName, lastname, username, password, email) values('$name','$lname','$username','$password','$email');";
+                $sql = "insert into ParkInLot_Users (firstName, lastname, username, password, email, make, model, year, color, licensePlate) values('$name','$lname','$username','$password','$email', '$make', '$model', '$year', '$color', '$licensePlate');";
                 $db->query($sql);
 
                 $_SESSION['active'] = true;
@@ -79,6 +96,12 @@ if (!empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['username
                 $_SESSION['access'] = $row['access'];
                 $_SESSION['email'] = $row['email'];
                 $_SESSION['username'] = $row['username'];
+
+                $_SESSION['make'] = $row['make'];
+                $_SESSION['model'] = $row['model'];
+                $_SESSION['year'] = $row['year'];
+                $_SESSION['color'] = $row['color'];
+                $_SESSION['licensePlate'] = $row['licensePlate'];
 
                 header('Location: login.php');
             }
@@ -117,6 +140,26 @@ else{
     if ( empty($_POST['confirm_password'])){
 
         $error .= 'CONFIRM PASSWORD<br>';
+    }
+    if (empty($_POST['make'])){
+
+        $error .= 'Make<br>';
+    }
+    if (empty($_POST['model'])){
+
+        $error .= 'model<br>';
+    }
+    if (empty($_POST['year'])){
+
+        $error .= 'year<br>';
+    }
+    if (empty($_POST['color'])){
+
+        $error .= 'color<br>';
+    }
+    if ( empty($_POST['licensePlate'])){
+
+        $error .= 'LicensePlate<br>';
     }
 
 
