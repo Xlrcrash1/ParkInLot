fb32b18697f52ff55afa7d3e4e11b97e39442c7b
twatson6 <twatson6@csub.edu>
2020-11-16 22:47:49 -0800

    Added 2 new files: loctest.php and phptest.php. These loctest is a php snippet we can use to get location information for users. The phptest.php is just a simple php snippet from when I was first practicing php.

---
+++ loctest.php
@@ -0,0 +1,26 @@
+/* Written By: Terry Watson
+ * Description:
+ * This php file uses a plugin called geoplugin
+ * to find the users country, city, longitude, and latitude.
+ * The longitude and latitude will most likely be helpful in the future
+ * while developing the app.
+<html>
+ <head>
+  <title>Geolocation Test</title>
+ </head>
+ <body>
+<?php  
+$user_ip = getenv('REMOTE_ADDR');
+$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
+$country = $geo["geoplugin_countryName"];
+$city = $geo["geoplugin_city"];
+$longitude = $geo["geoplugin_longitude"];
+$latitude = $geo["geoplugin_longitude"];
+echo "User country: " . $country;
+echo "\r\nUser city: " . $city . "<br>";
+echo "\r\nUser longitude: " . $longitude . "<br>";
+echo "\r\nUser latitude: " . $latitude . "<br>";
+
+?> 
+ </body>
+</html>
---
+++ phptest.php
@@ -0,0 +1,16 @@
+/*
+ * Written by: Terry Watson
+ * Description: Trying to understand the syntax and format for
+ * PHP. Pretty interesting overall. This phpinfo() function is
+ * really nice!
+ */
+
+<html>
+    <head>
+        <title>PHP Testing :P</title>
+        </head>
+        <body>
+        <?php echo '<p>Hello World</p>'; ?>
+        <?php phpinfo(); ?>
+        </body>
+</html>
