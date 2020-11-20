513ac395706de7ab338a701d91b0b5fb800ca248
twatson6 <twatson6@csub.edu>
2020-11-20 09:58:27 -0800

    Fixed an error in loctest.php and added to my log

--- loctest.php
+++ loctest.php
@@ -1,9 +1,10 @@
-/* Written By: Terry Watson
- * Description:
- * This php file uses a plugin called geoplugin
- * to find the users country, city, longitude, and latitude.
- * The longitude and latitude will most likely be helpful in the future
- * while developing the app.
+<!--Written By: Terry Watson
+//Description:
+//This php file uses a plugin called geoplugin
+//to find the users country, city, longitude, and latitude.
+//The longitude and latitude will most likely be helpful in the future
+//while developing the app.
+-->
 <html>
  <head>
   <title>Geolocation Test</title>
@@ -15,7 +16,7 @@ $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_i
 $country = $geo["geoplugin_countryName"];
 $city = $geo["geoplugin_city"];
 $longitude = $geo["geoplugin_longitude"];
-$latitude = $geo["geoplugin_longitude"];
+$latitude = $geo["geoplugin_latitude"];
 echo "User country: " . $country;
 echo "\r\nUser city: " . $city . "<br>";
 echo "\r\nUser longitude: " . $longitude . "<br>";
