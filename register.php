0483e4d56e128bacdb9e048432325b933690be52
Terry Watson <twatson@odin.cs.csubak.edu>
2020-12-04 19:04:54 -0800

    Got reCaptcha operational on the registration page. No more Spam accounts! -Terry

--- register.php
+++ register.php
@@ -7,6 +7,7 @@ echo "<html>\n";
 echo "  <head>\n";
 echo "      <title>REGISTER</title>\n";
 echo "      <link rel = 'stylesheet' type = 'text/css' href = 'style.css'>\n";
+echo "      <script src='https://www.google.com/recaptcha/api.js'></script>\n";
 echo "  </head>\n";
 echo "  <body>\n";
 
@@ -23,10 +24,11 @@ echo "              <h5>Confirm Password: <input type = 'password' placeholder =
 
 echo "              <h5>Make: <input type = 'text' placeholder = 'Toyota' name = 'make'></h5>\n";
 echo "              <h5>Model: <input type = 'text' placeholder = 'GT86' name = 'model'></h5>\n";
-echo "              <h5>2017: <input type = 'number' placeholder = '2017' name = 'year'></h5>\n";
+echo "              <h5>Year: <input type = 'number' placeholder = '2017' name = 'year'></h5>\n";
 echo "              <h5>Last 4 of LicensePlate: <input type = 'text' placeholder = 'C777' name = 'licensePlate'></h5>\n";
 echo "              <h5>Color: <input type = 'text' placeholder = 'Grey' name = 'color'></h5>\n";
 echo "              <h5>Photo: <input type = 'text' placeholder = 'asdf.jpg' name = 'photo'></h5>\n";
+echo "              <div class='g-recaptcha' data-sitekey='6LcdvfkZAAAAANZYnLTRvlsXFYDtim_Kz33h16m5'></div>";
 echo "              <button class = 'nav_btn' type = 'submit'>Create</button>\n";
 echo "          </form>\n";
 echo "      </div>\n";
@@ -34,8 +36,17 @@ echo "      </div>\n";
 echo "  </body>\n";
 echo "</html>\n";
 
+//echo "<script>\n";
+//echo "function checkCaptcha(){\n";
+//echo "var response = grecaptcha.getResponse();\n";
+//echo "if (response.length == 0)\n";
+//echo "{ alert('Please verify you are human!')\n";
+//echo "return false;\n}\n";
+//echo "else{return true;}\n}\n";
+//echo "</script>\n";
 
-if (!empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['userName']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])){
+
+if (($_POST["g-recaptcha-response"] != '') && !empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['userName']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])){
 
     //this converts the post parameters from HTML into PHP parameters as well as we sanatize it to avoid sql queries being interjected
     $name = htmlspecialchars(trim($_POST['name']));/////////////////ADD SANATIZATION
@@ -59,7 +70,7 @@ if (!empty($_POST['name']) && !empty($_POST['lname']) && !empty($_POST['userName
     //$confirm_password = $_POST['confirm_password'];
 
     //echo "name: $name , lastname: $lname, userName: $userName , email: $email , password: $password , confirm: $confirm_password, Make: $make, Model = $model, Year: $year, licenseplate: $licensePlate";
-
+    
     if ($password == $confirm_password){
 
         $sql = "select * from Users where userName = '$userName' or email = '$email' or licensePlate = '$licensePlate'";
