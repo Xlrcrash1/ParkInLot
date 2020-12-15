f76b90dba06c7d612612104fdf1d5d5c5a1d39bf
Xlrcrash1 <60253517+Xlrcrash1@users.noreply.github.com>
2020-12-15 10:35:52 -0800

    fixing post request for database.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if ($_SESSION['active'] == true){
         echo "              <div id = 'Database_Query_Options'>\n";
         echo "              Which table would you like to look at?<br> \n";
         
-        echo "                  <form action = './database.php' target='_blank' method='post'>\n";
+        echo "                  <form action = 'database.php' target='_blank' method='post'>\n";
         echo "                       <input type='radio' id='UsersTable' name='UsersTable' value='select * from Users;'>\n";
         echo "                      <label for='UsersTable'>UsersTable</label><br><br>\n";
         echo "                      <input type ='submit' value = 'Submit'>\n";
