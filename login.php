75af91eb549eb308398a72198e2505354485753a
Xlrcrash1 <60253517+Xlrcrash1@users.noreply.github.com>
2021-02-27 14:00:55 -0800

    switching style of input - temp

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ include_once('nav.php');
 echo "      <div class = 'container'>\n";      
 echo "          <form method = 'POST'>\n";
 //echo "              <input type = 'Uname' placeholder = 'User Name or Email' name = 'Uname'>\n";
-echo "              <input class = 'w3-input' type = 'text'>\n";
+echo "              <input class = 'w3-input' type = 'Uname' placeholder = 'UserName or Email' name = 'Uname'>\n";
 echo "              <input type = 'password' placeholder = 'Password' name = 'password'>\n";
 echo "              <button class = 'nav_btn' type = 'submit'>Login</button\n";
 //echo "          <button type = "checkbox" checked = "checked" name = 'remeber'>Remember me
