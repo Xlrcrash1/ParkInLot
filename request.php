d69a7f029e64b81f4816663628377bf86b5ff3d3
David Montes De Oca <davidxmontes@gmail.com>
2021-03-13 17:10:35 -0800

    Added request.php where users can request a spot. Currently outputs list of available spots

---
+++ request.php
@@ -0,0 +1,67 @@
+<?php
+
+session_start();
+if ($_SESSION['active'] == false){
+
+    header('location: login.php'); exit();
+}
+
+?>
+
+<!DOCTYPE = HTML>
+<html>
+    <head>
+        <title>ParkInLot</title>
+    <?php 
+        require('SQLconnect.php');
+        include('./CSS/bootStrap.html');
+    ?>
+    <link rel = 'stylesheet' type = 'text/css' href = './CSS/style.css'>
+
+    </head>
+    <body>
+        <?php
+            if ($_SESSION['active'] == true){   
+                include('nav.php'); 
+        ?>
+
+    <div id='status'>
+        <?php
+            if ($_SESSION['tokens'] > 0){ ?>
+            <div class="alert alert-info" id="request_status">
+                <strong>We're looking for a spot! </strong>You have <strong>
+                    <?php echo $_SESSION['tokens']; ?> token(s)</strong> in your account.
+                <br>Please click <strong>Cancel</strong> if you would like to cancel your request
+            </div>
+            
+        <?php
+            // Check for available spot
+            $sql = "SELECT userID, userName, parkingLot, Spots.time 
+                    FROM SpotsDetails 
+                    INNER JOIN Spots ON userID = pUserID 
+                    WHERE rUserID IS NULL 
+                    ORDER BY time ASC;";
+
+            $res = $db->query($sql);
+            echo "<p>Available Spots</p>";
+
+            while($row = $res->FETCH_ASSOC())
+            {
+               echo "User Name: {$row['userName']} ---------- Parking Lot: {$row['parkingLot']}";
+               echo "<br>";
+            }
+
+
+            } else { ?>
+            <div class="alert alert-info">
+                <strong>Sorry you do not have any tokens in your account.</strong> You can earn tokens by offering parking spots.
+            </div>
+        <?php
+        } ?>
+    </div>
+        <?php 
+            }
+            include('./javaScript/javaScript.html');  
+        ?>
+    </body>
+</html>
